79567fca7a1d53330f27e27d6fe0efc3ce774b57
zkzoomer <sergio@zkzoomer.io>
2023-11-18 19:47:23 +0300

    update utils

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -50,7 +50,7 @@ export class Board {
         return Field.fromBits(isPlayed.concat(player));
     };
   
-    update(x: Field, y: Field, playerToken: Bool) {
+    update(x: Field, y: Field, isTurnA: Bool) {
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
                 // is this the cell the player wants to play?
@@ -62,80 +62,85 @@ export class Board {
                 // copy the board (or update if this is the cell the player wants to play)
                 this.board[i][j] = Provable.if(
                     toUpdate,
-                    new OptionalBool(true, playerToken),
+                    new OptionalBool(true, isTurnA),
                     this.board[i][j]
                 );
             };
         };
     };
-};
 
-const emptyBoard = [9, 9, 9, 9, 9, 9, 9, 9, 9];
-const N = emptyBoard.length;  // Number of squares of the board
+    printState() {
+        for (let i = 0; i < 3; i++) {
+            let row = '| ';
+            for (let j = 0; j < 3; j++) {
+                let token = '_';
 
-function boardToString(board: number[]) {
-    let encodedBoard = 0;
-
-    for (let i = 0; i < N; i++) {
-        encodedBoard += board[N - i - 1] * 10 ** i;
+                if (this.board[i][j].isSome.toBoolean()) {
+                    token = this.board[i][j].value.toBoolean() ? 'X' : 'O';
+                };
+        
+                row += token + ' | ';
+            };
+            console.log(row);
+        };
+        console.log('---\n');
     };
 
-    return encodedBoard.toString();
-};
-
-function boardToField(board: number[]) {
-    return Field(boardToString(board));
-}
-
-function boardTransitionToField(startBoard: number[], endBoard: number[]) {
-    return Field(boardToString(startBoard) + boardToString(endBoard));
-};
-
-function isWinningState(board: number[], isPlayerA: boolean) {
-    const winningCombinations = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6] 
-    ];
-
-    const player = isPlayerA ? 1 : 2;
-
-    for (var winningCombination of winningCombinations) {
-        var count = 0;
-
-        for (var index of winningCombination) {
-            if (board[index] !== player) break; // don't bother looking further.
-            count++;
+    checkWin(): Bool {
+        let won = new Bool(false);
+    
+        // check rows
+        for (let i = 0; i < 3; i++) {
+            let row = this.board[i][0].isSome;
+            row = row.and(this.board[i][1].isSome);
+            row = row.and(this.board[i][2].isSome);
+            row = row.and(this.board[i][0].value.equals(this.board[i][1].value));
+            row = row.and(this.board[i][1].value.equals(this.board[i][2].value));
+            won = won.or(row);
         }
-
-        if (count === 3) return true;
+    
+        // check cols
+        for (let i = 0; i < 3; i++) {
+            let col = this.board[0][i].isSome;
+            col = col.and(this.board[1][i].isSome);
+            col = col.and(this.board[2][i].isSome);
+            col = col.and(this.board[0][i].value.equals(this.board[1][i].value));
+            col = col.and(this.board[1][i].value.equals(this.board[2][i].value));
+            won = won.or(col);
+        }
+    
+        // check diagonals
+        let diag1 = this.board[0][0].isSome;
+        diag1 = diag1.and(this.board[1][1].isSome);
+        diag1 = diag1.and(this.board[2][2].isSome);
+        diag1 = diag1.and(this.board[0][0].value.equals(this.board[1][1].value));
+        diag1 = diag1.and(this.board[1][1].value.equals(this.board[2][2].value));
+        won = won.or(diag1);
+    
+        let diag2 = this.board[0][2].isSome;
+        diag2 = diag2.and(this.board[1][1].isSome);
+        diag2 = diag2.and(this.board[0][2].isSome);
+        diag2 = diag2.and(this.board[0][2].value.equals(this.board[1][1].value));
+        diag2 = diag2.and(this.board[1][1].value.equals(this.board[2][0].value));
+        won = won.or(diag2);
+    
+        //
+        return won;
     };
 
-    return false;
-};
+    checkDraw(): Bool {
+        if (this.checkWin()) return Bool(false); 
 
-function isDrawState(board: number[]) {
-    if (isWinningState(board, true) || isWinningState(board, false)) return false;
+        let draw =      this.board[0][0].isSome;
+        draw = draw.and(this.board[0][1].isSome);
+        draw = draw.and(this.board[0][2].isSome);
+        draw = draw.and(this.board[1][0].isSome);
+        draw = draw.and(this.board[1][2].isSome);
+        draw = draw.and(this.board[1][2].isSome);
+        draw = draw.and(this.board[2][0].isSome);
+        draw = draw.and(this.board[2][1].isSome);
+        draw = draw.and(this.board[2][2].isSome);
 
-    for (var square of board) {
-        if (square === 9) return false;
+        return draw;
     };
-
-    return true;
 };
-
-function boardTransition(board: number[], isTurnA: boolean, index: number) {
-    if (board[index] !== 9) return null;
-
-    const newBoard = [...board]
-    newBoard[index] = isTurnA ? 1 : 2;
-
-    return newBoard;
-};
-
-export { emptyBoard, N, boardToString, boardToField, boardTransitionToField, isWinningState, isDrawState, boardTransition }
